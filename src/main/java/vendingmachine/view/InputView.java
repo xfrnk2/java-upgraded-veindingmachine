192680ca162492dcec38813a45c6072467db4072
xfrnk2 <xfrnk2@gmail.com>
2021-12-15 21:09:20 +0900

    refactor(InputView): 접근제한자 수정, 매직 넘버 상수화

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -3,13 +3,14 @@ package vendingmachine.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-	public static final String ERROR_NON_DIGIT_AMOUNT = "금액은 숫자여야 합니다.";
-	public static final String ERROR_NOT_UPPER_THAN_ZERO = "금액은 0이상의 숫자여야 합니다.";
+	private static final String ERROR_NON_DIGIT_AMOUNT = "금액은 숫자여야 합니다.";
+	private static final String ERROR_NOT_UPPER_THAN_ZERO = "금액은 0이상의 숫자여야 합니다.";
+	public static final int AMOUNT_LOWER_BOUND = 0;
 
 	public int scanAmount() {
 		try {
 			int number = Integer.parseInt(Console.readLine());
-			if (number < 0) {
+			if (number < AMOUNT_LOWER_BOUND) {
 				throw new IllegalArgumentException(ERROR_NOT_UPPER_THAN_ZERO);
 			}
 			return number;
