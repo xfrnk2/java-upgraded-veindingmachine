d9124894e77b09b29eb927e1de8d5ee34b152382
xfrnk2 <xfrnk2@gmail.com>
2021-12-16 19:59:07 +0900

    refactor: 각 상품명, 가격, 수량이 적힌 상품 목록을 입력받아 등록시 예외처리 구현
    - 상품 상세를 콤마(,)로 나누었을 때 상품명, 가격, 수량의 총 세단위가 아니면 예외처리

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -11,9 +11,11 @@ public class Product {
 	private static final int NUMBER_INDEX = 2;
 	private static final int COST_LOWER_BOUND = 100;
 	private static final int NUMBER_LOWER_BOUND = 0;
+	private static final int DETAILS_CONTAINER_SIZE = 3;
 	private static final String COST = "가격";
 	private static final String NUMBER = "수량";
 
+	private static final String ERROR_INVALID_DETAILS_CONTAINER_SIZE = "상품 상세는 상품명, 가격, 수량 세 단위여야 합니다.";
 	private static final String ERROR_NON_DIGIT = "%s의 %s이 숫자여야 합니다.";
 	private static final String ERROR_COST_NOT_DIVISIBLE = "%s의 가격이 %d으로 나누어 떨어져야 합니다.";
 	private static final String ERROR_INVALID_RANGE_LOWER_BOUND = "%s의 %s이 %d 이상이여야 합니다.";
@@ -23,6 +25,7 @@ public class Product {
 	private int number;
 
 	public Product(final List<String> details) {
+		validateSize(details);
 		this.name = details.get(NAME_INDEX);
 		this.cost = isDigit(details.get(COST_INDEX), COST);
 		this.number = isDigit(details.get(NUMBER_INDEX), NUMBER);
@@ -36,6 +39,12 @@ public class Product {
 			return Integer.parseInt(cost);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(String.format(ERROR_NON_DIGIT, name, type));
+		}
+	}
+
+	private void validateSize(List<String> details) {
+		if (DETAILS_CONTAINER_SIZE != details.size()) {
+			throw new IllegalArgumentException();
 		}
 	}
 
